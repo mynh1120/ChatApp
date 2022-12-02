428b87e3dbec61e14d039a8c8534fff937d0884d
Mynhh1120 <Mynhh1120@NgDuyMinh>
2022-12-02 14:27:55 +0700

    Init Project

---
+++ chat-server/src/main/java/learn/demo/chat/server/ChatServer.java
@@ -0,0 +1,13 @@
+
+package learn.demo.chat.server;
+
+/**
+ *
+ * @author Ngô Duy Minh
+ */
+public class ChatServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
